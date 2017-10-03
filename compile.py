8802a539799baebe2f21257583929423e5cec54a
DormantMan <rusl.stepanov2014@yandex.ru>
2017-10-03 08:46:48 +0300

    Update compile.py

--- compile.py
+++ compile.py
@@ -4,16 +4,6 @@ import time
 yl = YandexLyceum()
 yl.load_cookies()
 
-if not yl.get_status():
-    print('Username: ', end='')
-    username = input()
-
-    print('Password: ', end='')
-    password = input()
-
-    if yl.auth(username, password):
-        yl.save_cookies()
-
 
 
 yl.profile()
@@ -26,26 +16,12 @@ yl.parse_tasks(input('From: '), input('To: '))
 #yl.parse_tasks(10400, 10500)
 
 
-
 yc = YandexContest()
 yc.load_cookies()
 
 
-if not yc.get_status():
-    print('Username: ', end='')
-    username = input()
-
-    print('Password: ', end='')
-    password = input()
-
-    if yc.auth(username, password):
-        yc.save_cookies()
-
-
 start = time.clock()
-
 yc.parse(input('From: '), input('To: '))
-
 print('Time: %s' % (time.clock()-start))
 
 
