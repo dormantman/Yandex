16c8308c25d58aac79a4640b582ace652c5fa40c
Семён <semen.say@ya.ru>
2017-10-03 13:04:37 +0300

    Сделал нормальный таймер.

--- compile.py
+++ compile.py
@@ -1,9 +1,8 @@
 from yandex import YandexLyceum, YandexContest
 import time
-
+import sys
 yl = YandexLyceum()
 yl.load_cookies()
-
 
 
 yl.profile()
@@ -22,7 +21,13 @@ yc.load_cookies()
 
 start = time.clock()
 yc.parse(input('From: '), input('To: '))
-print('Time: %s' % (time.clock()-start))
 
-
-input('Press Enter to Exit...')
+while True:
+    try:
+        print('\b' * 100, end='')
+        print('Time: %s' % (time.clock()-start), "\t Press Ctrl+D or Ctrl+C to exit.", end='')
+        sys.stdout.flush()
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print("Exiting...")
+        break
