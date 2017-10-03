881f4d53539df452aa57ce222a37763f6ad220e9
DormantMan <rusl.stepanov2014@yandex.ru>
2017-10-03 19:02:57 +0300

    Update compile.py

--- compile.py
+++ compile.py
@@ -9,21 +9,26 @@ yl.profile()
 print(' -- Parse Lessons --')
 if yl.get_status:
     yl.parse_lessons(input('From: '), input('To: '))
+
 else:
     print('You are already authorized.')
 
 print(' -- Parse Tasks --')
 if yl.get_status:
     yl.parse_tasks(input('From: '), input('To: '))
+
 else:
     print('You are already authorized.')
 
 yc = YandexContest()
 
+yc.profile()
+
 print(' -- Parse Contests --')
 start = time.clock()
 if yc.get_status:
     yc.parse(input('From: '), input('To: '), input('Word: '))
+
 else:
     print('You are already authorized.')
     exit()
@@ -37,4 +42,3 @@ while True:
     except KeyboardInterrupt:
         print("Exiting...")
         break
-        
